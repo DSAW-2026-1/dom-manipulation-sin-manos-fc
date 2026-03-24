237c81b1f4fb127e19935588fb21f2917e90b811
Alyth <134451888+Andrew5331@users.noreply.github.com>
2026-03-24 07:41:10 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,41 @@
-console.log("JavaScript working!");
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", addTask);
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+
+  // Checkbox
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  checkbox.addEventListener("change", () => {
+    li.classList.toggle("completed");
+  });
+
+  // Texto
+  const span = document.createElement("span");
+  span.textContent = taskText;
+
+  // Botón eliminar
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Eliminar";
+
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
+  li.appendChild(checkbox);
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
+
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+}
